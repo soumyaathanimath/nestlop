17bb9b98aa7b1f930ef92ae5ef093f41da3da0e7
soumyaathanimath <>
2024-05-16 10:51:12 +0530

    Nested loops

---
+++ src/nestlop/Nestlop.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nestlop;
+
+/**
+ *
+ * @author dell1
+ */
+public class Nestlop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=7;j++)
+            {
+                System.out.print("("+i+","+j+") ");
+                
+            }
+            System.out.println("");
+        }
+    
+    }   
+    }
